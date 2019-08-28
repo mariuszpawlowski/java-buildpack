e7b1badd680b16cf1b5758c7f813416b4fc024dd
Mariusz Pawlowski <mariusz.pawlowski@merapar.com>
2019-08-28 09:39:01 +0200

    java 8

--- lib/language_pack/java.rb
+++ lib/language_pack/java.rb
@@ -4,7 +4,7 @@ require "fileutils"
 module LanguagePack
   class Java
 
-    DEFAULT_JDK_VERSION = "1.11".freeze
+    DEFAULT_JDK_VERSION = "1.8".freeze
     JDK_URL_1_6 = "https://s3.amazonaws.com/heroku-jvm-langpack-java/openjdk6-u25-heroku-temaki.tar.gz".freeze
     JDK_URL_1_7="https://s3.amazonaws.com/heroku-jvm-langpack-java/openjdk7-u7-heroku-temaki-b30.tar.gz".freeze
     JDK_URL_1_8="https://intiuss-mp.s3-eu-west-1.amazonaws.com/jdk8.tar.gz".freeze
